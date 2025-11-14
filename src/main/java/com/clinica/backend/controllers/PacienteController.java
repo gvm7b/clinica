705d37b7d81d1c23dfb908c0745c91c2c0eed2ba
NEVI0 <neviocostamagagnin@gmail.com>
2025-11-13 22:09:34 -0300

    feat: adicionando o paciente controller

---
+++ src/main/java/com/clinica/backend/controllers/PacienteController.java
@@ -0,0 +1,44 @@
+package com.clinica.backend.controllers;
+
+import com.clinica.backend.dto.PacienteDTO;
+import com.clinica.backend.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<PacienteDTO>> getAllPacientes() {
+        return ResponseEntity.ok(pacienteService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteDTO> getPacienteById(@PathVariable UUID id) {
+        return ResponseEntity.ok(pacienteService.getOne(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> createPaciente(@RequestBody PacienteDTO pacienteDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.create(pacienteDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteDTO> updatePaciente(@PathVariable UUID id, @RequestBody PacienteDTO pacienteDTO) {
+        return ResponseEntity.ok(pacienteService.update(id, pacienteDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePaciente(@PathVariable UUID id) {
+        pacienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
