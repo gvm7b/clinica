4eaac8da886a63e033a4bc934fa431fd09180546
unknown <guivict233@gmail.com>
2025-11-23 21:27:59 -0300

    medicoController adicionado

---
+++ src/main/java/com/clinica/backend/controllers/MedicoController.java
@@ -0,0 +1,45 @@
+package com.clinica.backend.controllers;
+
+import com.clinica.backend.dto.MedicoDTO;
+import com.clinica.backend.service.MedicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/medico")
+public class MedicoController {
+
+    @Autowired
+    private MedicoService medicoService;
+
+    @GetMapping
+    public ResponseEntity<List<MedicoDTO>> getAllMedicos() {
+        return ResponseEntity.ok(medicoService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MedicoDTO> getMedicoById(@PathVariable UUID id){
+        return ResponseEntity.ok(medicoService.getOne(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicoDTO> createMedico(@RequestBody MedicoDTO medicoDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.create(medicoDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MedicoDTO> updateMedico(@PathVariable UUID id, @RequestBody MedicoDTO medicoDTO){
+        return ResponseEntity.ok(medicoService.update(id, medicoDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMedico(@PathVariable UUID id) {
+        medicoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/clinica/backend/service/MedicoService.java
+++ src/main/java/com/clinica/backend/service/MedicoService.java
@@ -1,9 +1,7 @@
 package com.clinica.backend.service;
 
 import com.clinica.backend.dto.MedicoDTO;
-import com.clinica.backend.dto.PacienteDTO;
 import com.clinica.backend.entities.Medico;
-import com.clinica.backend.entities.Paciente;
 import com.clinica.backend.repository.MedicoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +27,12 @@ public class MedicoService {
     }
 
 
-    private MedicoDTO getOne(UUID id) {
+    public MedicoDTO getOne(UUID id) {
         Medico medico = medicoRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Medico não encontrado."));
         return toDTO(medico);
     }
 
-    private List<MedicoDTO> getAll() {
+    public List<MedicoDTO> getAll() {
         return medicoRepository.findAll()
                 .stream()
                 .map(this::toDTO)
@@ -42,19 +40,19 @@ public class MedicoService {
     }
 
 
-    private MedicoDTO create(MedicoDTO dto) {
+    public MedicoDTO create(MedicoDTO dto) {
         Medico medico = toEntity(dto);
         return toDTO(medicoRepository.save(medico));
     }
 
 
-    private MedicoDTO update(UUID id, MedicoDTO dto) {
+    public MedicoDTO update(UUID id, MedicoDTO dto) {
         Medico medico = medicoRepository.findById(id)
                 .orElseThrow(()-> new EntityNotFoundException("Medico não encontrado."));
 
-       medico.setNome(dto.getNome());
-       medico.setCrm(dto.getCrm());
-       medico.setEspecialidade(dto.getEspecialidade());
+        medico.setNome(dto.getNome());
+        medico.setCrm(dto.getCrm());
+        medico.setEspecialidade(dto.getEspecialidade());
 
         return toDTO(medicoRepository.save(medico));
     }
